e4d616bff0476fb68a13195471325fc30036ffbc
NoeDenise <noelialombardo@hotmail.com>
2023-10-12 17:06:18 -0300

    js: oculta de la list la tarea que se esta ejecutando, muestra las pendientes

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const tasks = []
 let time = 0
 let timer = null
-let ak = null
+let task = null
 let current = null
+let currentTaskId = null;
 
 const bAdd = document.querySelector("#bAdd")
 const itTask = document.querySelector("#itTask")
@@ -31,57 +32,67 @@ function createTask(value){
     tasks.unshift(newTask)
 }
 
-function renderTasks(){
-    const html = tasks.map(task => {
-        return `
-            <div class="task">
-                <div class="completed"> ${
-                    task.completed 
-                        ? `<span class="done">"Completada"</span>` 
-                        : `<button class="start-button" data-id="${task.id}"><img src="./img/Group 818.png">Start</button>`
-                    }</div>
-                <div class="title">${task.title}</div>
+function renderTasks() {
+    const tasksContainer = document.querySelector("#tasks");
+    tasksContainer.innerHTML = ""; // Limpia el contenido anterior
+
+    tasks.forEach(task => {
+        if (task.id === currentTaskId) {
+            // No muestra la tarea actual en la lista
+            return;
+        }
+
+        const taskHtml = `
+            <div class="list-task">
+                <div class="task">
+                    <div class="completed">
+                        ${task.completed 
+                            ? `<span class="done">Completada</span>` 
+                            : `<button class="start-button" data-id="${task.id}"><img src="./img/Group 818.png">Start</button>`
+                        }
+                    </div>
+                    <div class="title">${task.title}</div>
+                </div>
             </div>
-        `
-    })
+        `;
+        
+        tasksContainer.innerHTML += taskHtml; // Agrega la tarea al contenedor
+    });
 
-    const tasksContainer = document.querySelector("#tasks")
-    tasksContainer.innerHTML = html.join(" ")
-
-    const startButtons = document.querySelectorAll(".task .start-button")
+    const startButtons = document.querySelectorAll(".task .start-button");
 
     startButtons.forEach(button => {
         button.addEventListener("click", e => {
-            if( !timer) {
-                const id = button.getAttribute("data-id")
-                startButtonHandler(id)
-                button.textContent= "En progreso..."
+            if (!timer) {
+                const id = button.getAttribute("data-id");
+                startButtonHandler(id);
+                button.textContent = "En progreso...";
             }
-        })
-    })
+        });
+    });
 }
 
-function startButtonHandler(id){
-    time = 25 * 60
-    current = id
-    const taskIndex = tasks.findIndex(task => task.id === id)
-    taskName.textContent = tasks[taskIndex].title
 
-      // Oculta el botón "Start"
-      const startButton = document.querySelector(`[data-id="${id}"]`);
-      startButton.style.display = "none";
+function startButtonHandler(id) {
+    time = 25 * 60;
+    currentTaskId = id; // Registra la tarea actual
+    const taskIndex = tasks.findIndex(task => task.id === id);
+    taskName.textContent = tasks[taskIndex].title;
 
-     // Oculta la tarea que se está ejecutando
-     const title = startButton.closest('.task').querySelector('.title');
-     title.style.display = "none";
-      
-  
-      // Agrega la clase al cuerpo de la página para cambiar el fondo
-      document.body.classList.add("active-background");
+    // Oculta el botón "Start" de la tarea actual
+    const startButton = document.querySelector(`[data-id="${id}"]`);
+    startButton.style.display = "none";
 
-    timer = setInterval( ()=> {
-        timeHandler(id)
-    }, 1000)
+    // Oculta solo la tarea que se está ejecutando
+    const taskContainer = startButton.closest('.list-task');
+    taskContainer.style.display = "none";
+
+    // Agrega la clase al cuerpo de la página para cambiar el fondo
+    document.body.classList.add("active-background");
+
+    timer = setInterval(() => {
+        timeHandler(id);
+    }, 1000);
 }
 
 function timeHandler(id){
@@ -99,13 +110,12 @@ function timeHandler(id){
     }
 }
 
-function startBreak(){
-    time = 5 * 60
-    taskName.textContent = "break"
-    timerBreak = setInterval(()=>{
-        timerBreakHandler()
-
-   }, 1000 )
+function startBreak() {
+    time = 5 * 60;
+    taskName.textContent = "break";
+    timerBreak = setInterval(() => {
+        timerBreakHandler();
+    }, 1000);
 }
 
 function timerBreakHandler() {
@@ -114,25 +124,28 @@ function timerBreakHandler() {
 
     if (time === 0) {
         clearInterval(timerBreak);
-        current = null;
+        currentTaskId = null; // Reinicia la tarea actual
         timerBreak = null;
         taskName.textContent = "";
 
-        // Restablece el fondo y muestra nuevamente el botón "Start"
+        // Restablece el fondo y muestra nuevamente el botón "Start" de la tarea actual
         document.body.classList.remove("active-background");
         renderTasks();
     }
 }
 
+function renderTime() {
+    const minutesDiv = document.querySelector("#minutes");
+    const secondsDiv = document.querySelector("#seconds");
 
-function renderTime(){
-    const timeDiv = document.querySelector("#time #value")
-    const minutes = parseInt(time/60)
-    const seconds = parseInt(time%60)
+    const minutes = parseInt(time / 60);
+    const seconds = parseInt(time % 60);
 
-    timeDiv.textContent = `${minutes < 10 ? "0": ""}${minutes}:${seconds < 10 ? "0" :""}${seconds}`
-
+    // Actualiza los divs de minutos y segundos
+    minutesDiv.textContent = minutes < 10 ? "0" + minutes : minutes;
+    secondsDiv.textContent = seconds < 10 ? "0" + seconds : seconds;
 }
+
 
 function markCompleted(id){
     const taskIndex = tasks.findIndex(task => task.id === id)
